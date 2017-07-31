e918dcb6260ebbeffcb32f59b6ec66fc93151ec6
Andy Cobaugh <andrewcobaugh@gmail.com>
2017-07-30 23:50:04 -0400

    finish statistic output, still working on error report

--- krb5perf.go
+++ krb5perf.go
@@ -3,19 +3,33 @@ package main
 import (
 	"fmt"
 	"github.com/alexflint/go-arg"
+	"github.com/montanaflynn/stats"
 	"github.com/zephyr-im/krb5-go"
 	"log"
 	"os"
 	"time"
 )
 
-// Usage: krb5perf.pl -k <keytab> -c <client princ> -s <server princ> -i <iterations> -p <parallelism> -h
 type Args struct {
 	Keytab      string `arg:"env:KTNAME,-k,required"`
 	Client      string `arg:"-c,required"`
 	Service     string `arg:"-s,required"`
 	Iterations  int    `arg:"-i,required"`
 	Parallelism int    `arg:"-p,required"`
+}
+
+type durations []time.Duration
+
+type authrequest struct {
+	keytab  *krb5.KeyTab
+	client  string
+	service string
+}
+
+type authresult struct {
+	success bool
+	err     error
+	elapsed time.Duration
 }
 
 func (Args) Version() string {
@@ -40,8 +54,6 @@ func main() {
 	authrequestc := make(chan authrequest, args.Iterations)
 	authresultc := make(chan authresult, args.Iterations)
 
-	var authresults = []authresult{}
-
 	// create workers
 	for i := 1; i <= args.Parallelism; i++ {
 		go authworker(i, authrequestc, authresultc)
@@ -54,34 +66,66 @@ func main() {
 	}
 
 	// collect results
+	var s durations
+	var f durations
+	var errors = make(map[error]int)
 	for i := 1; i <= args.Iterations; i++ {
-		authresults = append(authresults, <-authresultc)
+		r := <-authresultc
+		if r.success {
+			s = append(s, r.elapsed)
+		} else {
+			f = append(f, r.elapsed)
+			errors[r.err]++
+		}
 	}
 	elapsed := time.Since(start)
 
+	var error_report string
+	for e, i := range errors {
+		error_report += fmt.Sprintf("%d\t%s\n", i, e)
+	}
+
 	fmt.Printf("\n===========\n"+
-		"Total time elapsed: %s\n"+
-		"Iterations: %d, Parallelism: %d\n",
-		//	"SUCCESS: %d\n" +
-		//	"FAIL: %d\n" +
-		//	"SUCCESS time: avg: %d, max: %d, min: %d, 99pct: %d, 95pct %d\n" +
-		//	"FAIL time: avg: %d, max: %d, min: %d, 99pct: %d, 95pct %d\n" +
-		//	"\nFAIL reasons:\n%s",
+		"Total elapsed time: %s\n"+
+		"Parallelism: %d, SUCCESS/FAIL: %d/%d\n"+
+		"SUCCESS: avg: %s, max: %s, min: %s, 99pct: %s, 95pct: %s\n"+
+		"FAIL: avg: %s, max: %s, min: %s, 99pct: %s, 95pct: %s\n"+
+		"Errors:\n%s",
 		elapsed,
-		args.Iterations, args.Parallelism)
+		args.Parallelism, len(s), len(f),
+		s.dstat(stats.Mean), s.dstat(stats.Max), s.dstat(stats.Min), s.dpct(stats.Percentile, 99), s.dpct(stats.Percentile, 95),
+		f.dstat(stats.Mean), f.dstat(stats.Max), f.dstat(stats.Min), f.dpct(stats.Percentile, 99), f.dpct(stats.Percentile, 95),
+		error_report,
+	)
 
 }
 
-type authrequest struct {
-	keytab  *krb5.KeyTab
-	client  string
-	service string
+func (d durations) dstat(f func(stats.Float64Data) (float64, error)) time.Duration {
+	dfloat := make([]float64, len(d))
+	for i, v := range d {
+		dfloat[i] = float64(v)
+	}
+
+	s, err := f(dfloat)
+	if err != nil {
+		return 0
+	} else {
+		return time.Duration(s)
+	}
 }
 
-type authresult struct {
-	success bool
-	err     error
-	elapsed time.Duration
+func (d durations) dpct(f func(stats.Float64Data, float64) (float64, error), p float64) time.Duration {
+	dfloat := make([]float64, len(d))
+	for i, v := range d {
+		dfloat[i] = float64(v)
+	}
+
+	s, err := f(dfloat, p)
+	if err != nil {
+		return 0
+	} else {
+		return time.Duration(s)
+	}
 }
 
 func authworker(w int, authrequestc <-chan authrequest, authresultc chan<- authresult) {
